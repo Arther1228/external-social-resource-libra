0eafe982209485c358f285cbb8361bd52fd39779
LiangchuangYang <396986654@qq.com>
2021-08-30 18:03:35 +0800

    树机构表代码上传II

---
+++ src/main/java/com/suncreate/bigdata/washout/server/ComXqServer.java
@@ -0,0 +1,40 @@
+package com.suncreate.bigdata.washout.server;
+
+import com.suncreate.bigdata.washout.baseinfo.BaseServerInterface;
+import com.suncreate.bigdata.washout.model.libra.ComXqLibra;
+import com.suncreate.bigdata.washout.model.mysql.ComXqMysql;
+import com.suncreate.bigdata.washout.repository.libra.ComXqLibraRepository;
+import com.suncreate.bigdata.washout.repository.mysql.ComXqMysqlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Service
+public class ComXqServer implements BaseServerInterface<ComXqLibra, ComXqMysql> {
+
+    @Autowired
+    ComXqLibraRepository comXqLibraRepository;
+
+    @Autowired
+    ComXqMysqlRepository comXqMysqlRepository;
+
+    @Override
+    public Timestamp findMaxTime() {
+        return comXqLibraRepository.findMaxTime();
+    }
+
+    @Override
+    public Page<ComXqMysql> findAll(Pageable pageable, Timestamp startTime) {
+        return comXqMysqlRepository.findAll(pageable, startTime);
+    }
+
+    @Override
+    public void saveAll(List<ComXqLibra> targetList) {
+        comXqLibraRepository.saveAll(targetList);
+    }
+
+}
