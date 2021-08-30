a4106f73e86cf7dc6bd6b6b44840da61a917fca2
LiangchuangYang <396986654@qq.com>
2021-08-30 08:15:25 +0800

    更新两个实体类

---
+++ src/main/java/com/suncreate/bigdata/washout/model/libra/ComFwxxLibra.java
@@ -0,0 +1,154 @@
+package com.suncreate.bigdata.washout.model.libra;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "com_fwxx", schema = "vision_if", catalog = "shiny")
+public class ComFwxxLibra {
+    private String id;
+    private String appkey;
+    private String xqbm;
+    private String ldbm;
+    private String fwbm;
+    private String dyh;
+    private String mph;
+    private String fwztdm;
+    private String lch;
+    private String fwytdm;
+    private String cqxzdm;
+    private String fwmj;
+    private String bz;
+    private Date addTime;
+
+    @Id
+    @Column(name = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Column(name = "appkey")
+    public String getAppkey() {
+        return appkey;
+    }
+
+    public void setAppkey(String appkey) {
+        this.appkey = appkey;
+    }
+
+    @Column(name = "xqbm")
+    public String getXqbm() {
+        return xqbm;
+    }
+
+    public void setXqbm(String xqbm) {
+        this.xqbm = xqbm;
+    }
+
+    @Column(name = "ldbm")
+    public String getLdbm() {
+        return ldbm;
+    }
+
+    public void setLdbm(String ldbm) {
+        this.ldbm = ldbm;
+    }
+
+    @Column(name = "fwbm")
+    public String getFwbm() {
+        return fwbm;
+    }
+
+    public void setFwbm(String fwbm) {
+        this.fwbm = fwbm;
+    }
+
+    @Column(name = "dyh")
+    public String getDyh() {
+        return dyh;
+    }
+
+    public void setDyh(String dyh) {
+        this.dyh = dyh;
+    }
+
+    @Column(name = "mph")
+    public String getMph() {
+        return mph;
+    }
+
+    public void setMph(String mph) {
+        this.mph = mph;
+    }
+
+    @Column(name = "fwztdm")
+    public String getFwztdm() {
+        return fwztdm;
+    }
+
+    public void setFwztdm(String fwztdm) {
+        this.fwztdm = fwztdm;
+    }
+
+    @Column(name = "lch")
+    public String getLch() {
+        return lch;
+    }
+
+    public void setLch(String lch) {
+        this.lch = lch;
+    }
+
+    @Column(name = "fwytdm")
+    public String getFwytdm() {
+        return fwytdm;
+    }
+
+    public void setFwytdm(String fwytdm) {
+        this.fwytdm = fwytdm;
+    }
+
+    @Column(name = "cqxzdm")
+    public String getCqxzdm() {
+        return cqxzdm;
+    }
+
+    public void setCqxzdm(String cqxzdm) {
+        this.cqxzdm = cqxzdm;
+    }
+
+    @Column(name = "fwmj")
+    public String getFwmj() {
+        return fwmj;
+    }
+
+    public void setFwmj(String fwmj) {
+        this.fwmj = fwmj;
+    }
+
+    @Column(name = "bz")
+    public String getBz() {
+        return bz;
+    }
+
+    public void setBz(String bz) {
+        this.bz = bz;
+    }
+
+    @Column(name = "addtime")
+    public Date getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(Date addTime) {
+        this.addTime = addTime;
+    }
+}
+
---
+++ src/main/java/com/suncreate/bigdata/washout/model/mysql/ComRkxxMysql.java
@@ -0,0 +1,300 @@
+/**
+ * @filename:ComDict 2021年7月13日
+ * @project community-collect  V1.0
+ * Copyright(c) 2020 suncreate Co. Ltd.
+ * All right reserved.
+ */
+package com.suncreate.bigdata.washout.model.mysql;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "com_rkxx")
+public class ComRkxxMysql {
+    private String id;
+    private String appkey;
+    private String xqbm;
+    private String xm;
+    private String xbdm;
+    private String zjlxdm;
+    private String zjhm;
+    private String sjhm;
+    private String ryzp;
+    private String rklxdm;
+    private String csrq;
+    private String zzmmdm;
+    private String yxqqsrq;
+    private String yxqjzrq;
+    private String zjzp;
+    private String jgdm;
+    private String zjdxz;
+    private String hjdxz;
+    private String gjdm;
+    private String ywx;
+    private String ywm;
+    private String mzdm;
+    private String zjxydm;
+    private String  whcddm;
+    private String hyzkdm;
+    private String  byzkdm;
+    private String bz;
+
+    private Date addTime;
+
+    @Id
+    @Column(name = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Column(name = "appkey")
+    public String getAppkey() {
+        return appkey;
+    }
+
+    public void setAppkey(String appkey) {
+        this.appkey = appkey;
+    }
+
+    @Column(name = "xqbm")
+    public String getXqbm() {
+        return xqbm;
+    }
+
+    public void setXqbm(String xqbm) {
+        this.xqbm = xqbm;
+    }
+
+    @Column(name = "xm")
+    public String getXm() {
+        return xm;
+    }
+
+    public void setXm(String xm) {
+        this.xm = xm;
+    }
+
+    @Column(name = "xbdm")
+    public String getXbdm() {
+        return xbdm;
+    }
+
+    public void setXbdm(String xbdm) {
+        this.xbdm = xbdm;
+    }
+
+    @Column(name = "zjlxdm")
+    public String getZjlxdm() {
+        return zjlxdm;
+    }
+
+    public void setZjlxdm(String zjlxdm) {
+        this.zjlxdm = zjlxdm;
+    }
+
+    @Column(name = "zjhm")
+    public String getZjhm() {
+        return zjhm;
+    }
+
+    public void setZjhm(String zjhm) {
+        this.zjhm = zjhm;
+    }
+
+    @Column(name = "sjhm")
+    public String getSjhm() {
+        return sjhm;
+    }
+
+    public void setSjhm(String sjhm) {
+        this.sjhm = sjhm;
+    }
+
+    @Column(name = "ryzp")
+    public String getRyzp() {
+        return ryzp;
+    }
+
+    public void setRyzp(String ryzp) {
+        this.ryzp = ryzp;
+    }
+
+    @Column(name = "rklxdm")
+    public String getRklxdm() {
+        return rklxdm;
+    }
+
+    public void setRklxdm(String rklxdm) {
+        this.rklxdm = rklxdm;
+    }
+
+    @Column(name = "csrq")
+    public String getCsrq() {
+        return csrq;
+    }
+
+    public void setCsrq(String csrq) {
+        this.csrq = csrq;
+    }
+
+    @Column(name = "zzmmdm")
+    public String getZzmmdm() {
+        return zzmmdm;
+    }
+
+    public void setZzmmdm(String zzmmdm) {
+        this.zzmmdm = zzmmdm;
+    }
+
+    @Column(name = "yxqqsrq")
+    public String getYxqqsrq() {
+        return yxqqsrq;
+    }
+
+    public void setYxqqsrq(String yxqqsrq) {
+        this.yxqqsrq = yxqqsrq;
+    }
+
+    @Column(name = "yxqjzrq")
+    public String getYxqjzrq() {
+        return yxqjzrq;
+    }
+
+    public void setYxqjzrq(String yxqjzrq) {
+        this.yxqjzrq = yxqjzrq;
+    }
+
+    @Column(name = "zjzp")
+    public String getZjzp() {
+        return zjzp;
+    }
+
+    public void setZjzp(String zjzp) {
+        this.zjzp = zjzp;
+    }
+
+    @Column(name = "jgdm")
+    public String getJgdm() {
+        return jgdm;
+    }
+
+    public void setJgdm(String jgdm) {
+        this.jgdm = jgdm;
+    }
+
+    @Column(name = "zjdxz")
+    public String getZjdxz() {
+        return zjdxz;
+    }
+
+    public void setZjdxz(String zjdxz) {
+        this.zjdxz = zjdxz;
+    }
+
+    @Column(name = "hjdxz")
+    public String getHjdxz() {
+        return hjdxz;
+    }
+
+    public void setHjdxz(String hjdxz) {
+        this.hjdxz = hjdxz;
+    }
+
+    @Column(name = "gjdm")
+    public String getGjdm() {
+        return gjdm;
+    }
+
+    public void setGjdm(String gjdm) {
+        this.gjdm = gjdm;
+    }
+
+    @Column(name = "ywx")
+    public String getYwx() {
+        return ywx;
+    }
+
+    public void setYwx(String ywx) {
+        this.ywx = ywx;
+    }
+
+    @Column(name = "ywm")
+    public String getYwm() {
+        return ywm;
+    }
+
+    public void setYwm(String ywm) {
+        this.ywm = ywm;
+    }
+
+    @Column(name = "mzdm")
+    public String getMzdm() {
+        return mzdm;
+    }
+
+    public void setMzdm(String mzdm) {
+        this.mzdm = mzdm;
+    }
+
+    @Column(name = "zjxydm")
+    public String getZjxydm() {
+        return zjxydm;
+    }
+
+    public void setZjxydm(String zjxydm) {
+        this.zjxydm = zjxydm;
+    }
+
+    @Column(name = "whcddm")
+    public String getWhcddm() {
+        return whcddm;
+    }
+
+    public void setWhcddm(String whcddm) {
+        this.whcddm = whcddm;
+    }
+
+    @Column(name = "hyzkdm")
+    public String getHyzkdm() {
+        return hyzkdm;
+    }
+
+    public void setHyzkdm(String hyzkdm) {
+        this.hyzkdm = hyzkdm;
+    }
+
+    @Column(name = "byzkdm")
+    public String getByzkdm() {
+        return byzkdm;
+    }
+
+    public void setByzkdm(String byzkdm) {
+        this.byzkdm = byzkdm;
+    }
+
+    @Column(name = "bz")
+    public String getBz() {
+        return bz;
+    }
+
+    public void setBz(String bz) {
+        this.bz = bz;
+    }
+
+    @Column(name = "addtime")
+    public Date getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(Date addTime) {
+        this.addTime = addTime;
+    }
+}
